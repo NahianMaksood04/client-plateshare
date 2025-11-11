b6055aa1b1255b0cc9f975365b27880c263c2382
NahianMaksood04 <nahianmaksood04@gmail.com>
2025-11-11 17:10:43 +0600

    components

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+return (
+<div className="min-h-[50vh] flex items-center justify-center">
+<span className="loading loading-ring loading-lg text-primary"></span>
+</div>
+);
+};
+export default Loading;
