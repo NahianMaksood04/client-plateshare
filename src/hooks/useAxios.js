d5df85f5eecab929699070043f207c8423f1154f
NahianMaksood04 <nahianmaksood04@gmail.com>
2025-11-11 17:44:27 +0600

    hooks

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,20 +2,20 @@ import axios from "axios";
 import { auth } from "../services/firebase";
 
 const instance = axios.create({
-baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-withCredentials: false
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  withCredentials: false,
 });
 
 instance.interceptors.request.use(
-async (config) => {
-const u = auth.currentUser;
-if (u) {
-const token = await u.getIdToken();
-config.headers.Authorization = Bearer ${token};
-}
-return config;
-},
-(error) => Promise.reject(error)
+  async (config) => {
+    const u = auth.currentUser;
+    if (u) {
+      const token = await u.getIdToken();
+      config.headers.Authorization = `Bearer ${token}`; 
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default instance;
