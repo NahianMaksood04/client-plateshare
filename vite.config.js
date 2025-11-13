55f721b6917e7ff1089fcedfccdbe16ddfe5b244
NahianMaksood04 <nahianmaksood04@gmail.com>
2025-11-13 16:07:15 +0600

    13/11/2025 16:07

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-});
+  server: {
+    historyApiFallback: true, // fallback to index.html for SPA routes
+  },
+})
